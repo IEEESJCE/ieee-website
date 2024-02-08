22a800ebc6de5e5048709d45654089ef01beb475
SUBROTOKOIRY <koirysubroto1103@gmail.com>
2024-02-08 17:09:02 +0530

    updations

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -20,65 +20,66 @@ function Event() {
 
       <div className="new_event" id="new_event_1" data-aos="zoom-in">
         <img
-          src="/photos/events/kalpana_chawla_memorial_lecture.jpg"
+          src="/photos/events/python_bootcamp.jpg"
           alt=""
           data-aos="zoom-in"
         />
 
         <div className="inside_box">
-          <h1> Kalpana Chawla Memorial Lecture</h1>
+          <h1> Python Bootcamp</h1>
           <p data-aos="zoom-in">
-            Warm regards from IEEE-SJCE! ✨
+            Hola coders! 🙌
+            <br />
+
+            Greetings from IEEE-SJCE! ✨
             <br />
             <br />
-            Did the successful landing of Chandrayaan 3 fill you with pride?
-            Ever wondered about the challenges behind achieving such perfection?
-            🚀
-            <br />
-            To unravel these mysteries, IEEE-SJCE WIE wing proudly presents the
+            The RAS wing of IEEE-SJCE presents its annual event
             <br />
             <br />
-            🎤
-            <b style={{ color: '#0095da' }}>Kalpana Chawla Memorial Lecture</b>
-            🎤
+            <b style={{ color: '#0095da' }}>👨‍💻 Python Bootcamp  👩‍💻 </b>
             <br />
             <br />
-            <b style={{ color: '#0095da' }}>Mrs. Roopa M.V.</b>, an outstanding
-            scientist with an impressive background, who served as the{' '}
-            <b style={{ color: '#0095da' }}>
-              Deputy Project Director for Chandrayaan-3
-            </b>{' '}
-            Mission, will enlighten us with her profound insights on
-            <b style={{ color: '#0095da' }}>
-              "Challenges Faced in Chandrayaan-3 Mission"
-            </b>
-            .
+            Are you a student aiming to kickstart your programming journey? 🤩
             <br />
             <br />
-            🗓 Date: <b style={{ color: '#0095da' }}>Saturday, 9th December</b>
-            <br />⌚ Time: <b style={{ color: '#0095da' }}>2:30 PM</b>
+            The Python Bootcamp provides a well-organized approach for learning to program in Python. 👩‍💻
             <br />
-            📍 Venue: <b style={{ color: '#0095da' }}> IS Seminar Hall</b>
+            This bootcamp is designed for beginners seeking to develop strong coding skills. 🥳
             <br />
             <br />
-            Join us for an enriching session as we explore the complexities and
-            triumphs behind the Chandrayaan 3 Mission. 📡 See you all! 👋🏻
+            The ability to program in Python gives prospective developers access to a multitude of job opportunities, from scripting to data science. 🤯 
+            <br />
+            <br />
+            Join us and unlock the potential of Python for your coding journey!
+            <br />
+            <br />
+            <b style={{ color: '#0095da' }}>Registration is FREE! 🎉</b>
+            <br />
+            <br />
+            🗓 Date: <b style={{ color: '#0095da' }}>9th, 10th & 11th February</b>
+            <br />
+            📍 Platform : <b style={{ color: '#0095da' }}>Google Meet
+</b>
+            <br />
+            <br />
+            Sign up now! 🤩
             <br />
             <br />
             For any queries, contact:
             <br />
-            Sinchana: +91 <b style={{ color: '#0095da' }}> 9113024481</b>
+            Sudhanva: +91 <b style={{ color: '#0095da' }}> 7619618003</b>
             <br />
-            Vaibhav: +91 <b style={{ color: '#0095da' }}>9986340818</b>
+            Rishabh: +91 <b style={{ color: '#0095da' }}> 6201165099</b>
             <br />
           </p>
           <button className="btn">
             {' '}
             <a
               style={{ textDecoration: 'none', color: 'white' }}
-              href="https://bit.ly/Kalpana_Chawla_Memorial_Lecture_2023"
+              href="https://bit.ly/PYTHON_BOOTCAMP_2024"
             >
-              Watch liveStream 
+              Register Now 
             </a>
           </button>
         </div>
