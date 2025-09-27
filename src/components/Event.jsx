a4a15dfb1bed59ab9947d5dffd6d8c23593e26e4
ad-shekhar <adityashekhar0009@gmail.com>
2025-09-27 17:28:03 +0530

    event updated

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,14 +1,16 @@
-import {React,useEffect} from 'react'
+import { React, useEffect } from 'react'
 import '../css/event.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+
 function Event() {
-   useEffect(() => {
-     AOS.init({
-       duration: 900, // Set the duration of the animation
-       once: true, // Only animate once
-     })
-   }, [])
+  useEffect(() => {
+    AOS.init({
+      duration: 900, // Set the duration of the animation
+      once: true, // Only animate once
+    })
+  }, [])
+
   return (
     <div>
       <div className="event" id="event_part">
@@ -18,58 +20,62 @@ function Event() {
         <hr className="hr-1" />
       </div>
 
+      {/* ------------ Event 1 ------------ */}
       <div className="new_event" id="new_event_1" data-aos="zoom-in">
-      <img
-        src="/photos/events/tech_kshetra.jpg"
-        alt="tech_kshetra"
-        data-aos="zoom-in"
-      />
-
+        <img
+          src="photos/events/sps_inaguration.jpg"
+          alt="ieee_day"
+          data-aos="zoom-in"
+        />
 
         <div className="inside_box">
-          <h1>TECH_KSHETRA(Project Expo)</h1>
+          <h1>IEEE DAY 2025 & SPS Inauguration</h1>
           <p data-aos="zoom-in">
-            The RAS wing of IEEE-SJCE, in collaboration with the IEEE Mysore Subsection, is here with an inter-college project expo like no other!,<br />⚙ TechKshetra ⚙
-
-
+            This day honours the historic moment in 1884 when engineers from across
+            the globe first gathered to share ideas, collaborate, and spark
+            innovation. IEEE brings people and technology together to make a real
+            impact in the world. 🌍
             <br />
             <br />
-            Categories:<br/>
-🔸 Category I (1st & 2nd Year Students) – This category focuses on nurturing foundational innovation and promoting technical curiosity and a competitive spirit among early-stage undergraduate students.<br/>
-
-🔸 Category II (3rd & 4th Year Students ) – It is aimed at providing an advanced platform for students to present their refined, impactful projects and push the boundaries of their technical expertise.
-
+            As part of the IEEE Day celebration, this year we proudly launch the
+            Signal Processing Society. It is a great platform to explore signals in
+            healthcare, environment, and media through workshops and competitions
+            using MATLAB and Python. 👨‍💻
             <br />
             <br />
-            Important Details:<br/>
-🗓 Deadline for submission of team details: 29th April, 2025<br/>
-🗓 Project Expo date: 3rd May, 2025<br/>
-🏛 Venue: SJCE, JSS Science and Technology University, Mysore
-
-            <br />
-            <br />
-            Registration fee (for the selected teams):<br/>
-Team with atleast one IEEE member : Rs. 399/-<br/>
-Team without any IEEE member : Rs. 499/-<br/>
-
-            For more information, contact:
-            <br />
-            Nandeesh: <b style={{ color: '#0095da' }}>7483143962</b>
-            <br/>
-            Pranav: <b style={{ color: '#0095da' }}> 9110682462</b>
+            <b>Important Details:</b>
+            <br /> 🗓 Event date: 8th October, 2025
+            <br /> 🏛 Venue: SJCE, JSS Science and Technology University, Mysore
           </p>
-          {/* <button className="btn">
-            <a
-              style={{ textDecoration: 'none', color: 'white' }}
-              href="https://bit.ly/Soldering_and_Etching_Registration_form"
-            >
-              Register Now
-            </a>
-          </button> */}
+        </div>
+      </div>
+
+      {/* ------------ Event 2 ------------ */}
+      <div className="new_event" id="new_event_2" data-aos="zoom-in">
+        <img
+          src="photos/events/orientation_day.jpg"
+          alt="hackathon"
+          data-aos="zoom-in"
+        />
+
+        <div className="inside_box">
+          <h1>ORIENTATION DAY</h1>
+          <p data-aos="zoom-in">
+            IEEE-SJCE brings together passionate minds in technology, innovation, and creativity. This is your chance to explore the club and how you can be part of this exciting journey. 🚀
+            Interact with IEEE Execom members to learn about our various societies and get an inside look at the upcoming events in store for you! 🤝
+            <br />
+            <br />
+            This is more than just an introduction, it's your access to valuable connections and chance to engage with our lively community. 🌟
+            <br />
+            <br />
+            <b>Important Details:</b>
+            <br /> 🗓 Event date: 18th October, 2025
+            <br /> 🏛 Venue: IS Seminar Hall, JSS-STU
+          </p>
         </div>
       </div>
     </div>
   )
 }
 
-export default Event;
+export default Event
